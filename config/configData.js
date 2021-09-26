f1fb9653fd9f22e2a93833e3303a835d3318383a
Filipe Carvalho <69485332+Fi1ip-e@users.noreply.github.com>
2021-09-26 13:05:39 -0300

    removendoSenha

--- config/configData.js
+++ config/configData.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize({
     dialect: 'mysql',
     host: '127.0.0.1',
     username: 'root',
-    password: '01@Levi-28',
+    password: '',
     database: 'dbagendamento',
     define: {
       timestamps: false,
